48365ae203dce1e658e3cff950579fd976b22481
geeekbrains <127094889+geeekbrains@users.noreply.github.com>
2023-06-16 11:37:18 +0300

    Коллекция для практикума по Java
    Коллекция необходимая для задач: 11,12,13 практикума

---
+++ Java Workshop Collection (fruit).java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+  
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("apple");
+        arrayList.add("banana");
+        arrayList.add("cherry");
+        arrayList.add("watermelon");
+
+
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(1, "apple");
+        hashMap.put(2, "banana");
+        hashMap.put(3, "cherry");
+        hashMap.put(4, "watermelon");
+    
+        TreeSet<String> treeSet = new TreeSet<>();
+        treeSet.add("apple");
+        treeSet.add("banana");
+        treeSet.add("cherry");
+        treeSet.add("watermelon");
+        
+        System.out.println(result);
+    }
+}
